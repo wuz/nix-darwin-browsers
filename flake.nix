89ae4329e2cfd8f57023cdf31c71b13aaab97420
Atahan Yorgancı <atahanyorganci@hotmail.com>
2024-09-06 18:31:53 +0300

    feat: add default dev shell that fetches latest version of Firefox

--- flake.nix
+++ flake.nix
@@ -7,7 +7,31 @@
       eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
     in
     {
-      formatter = eachSystem (pkgs: pkgs.nixpkgs-fmt);
       overlay = import ./overlay.nix;
+      formatter = eachSystem (pkgs: pkgs.nixpkgs-fmt);
+      devShells = eachSystem (pkgs:
+        let
+          curl = "${pkgs.curl}/bin/curl";
+          jq = "${pkgs.jq}/bin/jq";
+          update-firefox-version = pkgs.writeShellScriptBin "update-firefox-version" ''
+            set -e
+            version=$(${curl} 'https://product-details.mozilla.org/1.0/firefox_versions.json' | ${jq} -r '.LATEST_FIREFOX_VERSION')
+            echo "Last version of Firefox is $version"
+            url="https://download-installer.cdn.mozilla.net/pub/firefox/releases/$version/mac/en-GB/Firefox%20$version.dmg"
+            sha256=$(nix-prefetch-url --name "Firefox-$version.dmg" $url)
+            ${jq} -n \
+              --arg version "$version" \
+              --arg sha256 "$sha256" \
+              --arg url "$url" \
+              '{version: $version, url: $url, sha256: $sha256}' > firefox.json
+          '';
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              update-firefox-version
+            ];
+          };
+        });
     };
 }
